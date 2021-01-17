70ad0cefa5346777b82db1da129e7e0feba388e8
ManPham1999 <manpham130699@gmail.com>
2021-01-17 14:18:24 +0700

    Update

--- KerryExample/Repository/ITransactionRepository.cs
+++ KerryExample/Repository/ITransactionRepository.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using KerryExample.Entity;
 
@@ -8,5 +9,6 @@ namespace KerryExample.Repository
     public interface ITransactionRepository
     {
         public Task<IEnumerable<Cart>> GetAllTransactions();
+        public Task<List<CartLine>> GetAllTransactionsPr(Guid guid);
     }
 }
--- KerryExample/Repository/TransactionRepository.cs
+++ KerryExample/Repository/TransactionRepository.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using KerryExample.Entity;
 using Microsoft.EntityFrameworkCore;
@@ -18,6 +19,12 @@ namespace KerryExample.Repository
         public async Task<IEnumerable<Cart>> GetAllTransactions()
         {
             return await _context.Carts.Include(c => c.CardType).Include(u => u.User).ToListAsync();
+        }
+
+        public async Task<List<CartLine>> GetAllTransactionsPr(Guid cartId)
+        {
+            var chosenPros = await _context.CartLines.Include(c=>c.Cart).Include(p => p.Product).Where(cl => cl.Cart.Id == cartId).ToListAsync();
+            return chosenPros;
         }
     }
 }
